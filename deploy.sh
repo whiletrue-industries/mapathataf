dc08c820cb458421936d14d17585541ea4b1cd2c
Adam Kariv <adam.kariv@gmail.com>
2025-03-11 20:35:36 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ cp CNAME dist/landing/browser/ || true && \
 git add dist/landing/browser && \
 git commit -m "Deploying landing browser files" && \
 (git branch -D gh-pages || true) && \
-cd / && \
+cd .. && \
 git subtree split --prefix landing/dist/landing/browser -b gh-pages && \
 git push -f origin gh-pages:gh-pages && \
 git checkout main && \
