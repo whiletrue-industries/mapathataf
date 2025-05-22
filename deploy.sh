3ad0bd549c660dfe848ba19b493565fa3d4d13d7
Adam Kariv <adam.kariv@gmail.com>
2025-05-22 20:07:47 +0300

    Reorg a bit before adding app

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,19 @@
 git checkout main && \
 (git branch -D dist || true) && \
 git checkout -b dist && \
-cd landing && \
+cd frontend && \
 rm .gitignore && \
-npm run build && \
-cp CNAME dist/landing/browser/ || true && \
-git add dist/landing/browser && \
-git commit -m "Deploying landing browser files" && \
+npm run build:landing && \
+npm run build:app && \
+mkdir _dist && \
+cp CNAME _dist || true && \
+mv dist/landing/browser/* _dist/ && \
+mv dist/app/browser _dist/a/ && \
+git add _dist && \
+git commit -m "Deploying frontend browser files" && \
 (git branch -D gh-pages || true) && \
 cd .. && \
-git subtree split --prefix landing/dist/landing/browser -b gh-pages && \
+git subtree split --prefix _dist -b gh-pages && \
 git push -f origin gh-pages:gh-pages && \
 git checkout main && \
 git branch -D gh-pages && \
