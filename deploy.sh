100aa87e080855edff78b1b14968836364b5578d
Adam Kariv <adam.kariv@gmail.com>
2025-05-22 20:21:06 +0300

    build

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ git add _dist && \
 git commit -m "Deploying frontend browser files" && \
 (git branch -D gh-pages || true) && \
 cd .. && \
-git subtree split --prefix _dist -b gh-pages && \
+git subtree split --prefix frontend/_dist -b gh-pages && \
 git push -f origin gh-pages:gh-pages && \
 git checkout main && \
 git branch -D gh-pages && \
