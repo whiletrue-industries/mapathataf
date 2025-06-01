200c1a1c6130e843a84728e5442dd4510ef0512f
Adam Kariv <adam.kariv@gmail.com>
2025-06-01 18:47:02 +0300

    Build for deployment

--- frontend/projects/admin/src/app/app.config.ts
+++ frontend/projects/admin/src/app/app.config.ts
@@ -1,13 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay, withHttpTransferCacheOptions } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes, withInMemoryScrolling({scrollPositionRestoration: 'enabled'})),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay(), withHttpTransferCacheOptions({
+      filter: (req) => false
+    })),
     provideHttpClient(withFetch()),
   ]
 };
