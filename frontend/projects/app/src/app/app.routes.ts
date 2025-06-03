a7bcf879e735a866439e6d373ee7c418813c8593
Adam Kariv <adam.kariv@gmail.com>
2025-06-03 12:12:44 +0300

    landing fixes + about page

--- frontend/projects/app/src/app/app.routes.ts
+++ frontend/projects/app/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
+    {
+        path: 'about/:section',
+        component: AboutComponent
+    },
     {
         path: ':workspaceId',
         component: MainComponent
--- frontend/projects/app/src/app/header/header.component.ts
+++ frontend/projects/app/src/app/header/header.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
+  imports: [
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.less'
 })
 export class HeaderComponent {
   active = false;
 
+  commonLinks = [
+    {
+      title: 'הגיל הרך, מאיפה מתחילים?',
+      routerLink: ['/about', 'early-childhood'],
+      // href: 'https://data-city.my.canva.site/tafmap-guidelines'
+    },
+    {
+      title: 'הדרכה ופיקוח מסגרות חינוך',
+      routerLink: ['/about', 'supervision'],    
+    },
+    {
+      title: 'צור קשר',
+      routerLink: ['/about', 'contact'],
+    },
+  ]
+
+  constructor(public api: ApiService) {}
 }
